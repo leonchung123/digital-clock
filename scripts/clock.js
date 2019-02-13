d9ae807cf08b1915e11adc9b173c33da21162b7e
Leon Chung <47351403+leonchung123@users.noreply.github.com>
2019-02-13 22:37:07 +1100

    Implemented time modes
    Added the ability to switch between military and 12 hour time displays using the mode dropdown menu.

--- scripts/clock.js
+++ scripts/clock.js
@@ -1,6 +1,9 @@
 // Leon Chung 2019
 // 24 hour clock (military time)
-function startTime() {
+var t;
+
+function startMilitaryTime() {
+    clearTimeout(t);
     var today = new Date();
     var hour = today.getHours();
     var minutes = today.getMinutes();
@@ -11,7 +14,29 @@ function startTime() {
 
     document.getElementsByClassName("time")[0].innerHTML = hour + ':' + minutes + ':' + seconds;
 
-    var t = setTimeout(startTime, 500);
+    t = setTimeout(startMilitaryTime, 500);
+}
+
+function start12HourTime() {
+    clearTimeout(t);
+    var today = new Date();
+    var hour = today.getHours();
+    var minutes = today.getMinutes();
+    var seconds = today.getSeconds();
+
+    minutes = checkTime(minutes);
+    seconds = checkTime(seconds);
+
+    if (hour == 0) {
+        hour = 12;
+    }
+    else if (hour > 12) {
+        hour = hour - 12;
+    }
+
+    document.getElementsByClassName("time")[0].innerHTML = hour + ':' + minutes + ':' + seconds + checkPeriod(hour);
+
+    t = setTimeout(start12HourTime, 500);
 }
 
 //if minutes or seconds < 10, we add a 0 in front for looks
